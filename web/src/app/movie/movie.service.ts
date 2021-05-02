2b2f8af15149d161f656591c982e6efff79af06c
rafaelvieirab <rafaelviebar@gmail.com>
2021-05-02 19:03:32 -0300

    feat: add movie-service

---
+++ web/src/app/movie/movie.service.ts
@@ -0,0 +1,59 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Genre, Movie, MoviesListResponse } from './models/movie';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor(private http: HttpClient) { }
+
+  getById(id: number): Observable<Movie> {
+    const url = `/movie/${id}`;
+    return this.http.get<Movie>(url);
+  }
+
+  getBySearch(query: string): Observable<MoviesListResponse> {
+    const url = `/search/movie?query=${query}`;
+    return this.http.get<MoviesListResponse>(url);
+  }
+
+  getMoviesGenres(): Observable<Genre[]>  {
+    const url = 'genre/movie/list';
+    return this.http.get<Genre[]>(url);
+  }
+
+  getRecommendationsById(id: number): Observable<MoviesListResponse> {
+    const url = `movie/${id}/recommendations`;
+    return this.http.get<MoviesListResponse>(url);
+  }
+
+  getSimilarById(id: number): Observable<MoviesListResponse> {
+    const url = `movie/${id}/similar`;
+    return this.http.get<MoviesListResponse>(url);
+  }
+
+  getPopular(): Observable<MoviesListResponse> {
+    const paramsUrl = '?language=pt-BR&page=1';
+    const url = `/movie/popular${paramsUrl}`;
+    return this.http.get<MoviesListResponse>(url);
+  }
+
+  getMoviesUpcoming(): Observable<MoviesListResponse> {
+    const url = '/movie/upcoming';
+    return this.http.get<MoviesListResponse>(url);
+  }
+
+  getMoviesNowPlaying(): Observable<MoviesListResponse> {
+    const url = '/movie/now_playing';
+    return this.http.get<MoviesListResponse>(url);
+  }
+
+  getMoviesTopRated(): Observable<MoviesListResponse> {
+    const url = '/movie/top_rated';
+    return this.http.get<MoviesListResponse>(url);
+  }
+}

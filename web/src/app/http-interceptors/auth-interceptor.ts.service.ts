ff25d321c5305edef40540e92cd1199d43c29bd6
rafaelvieirab <rafaelviebar@gmail.com>
2021-05-02 19:07:26 -0300

    feat: add http-interceptor

---
+++ web/src/app/http-interceptors/auth-interceptor.ts.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+
+import { the_movie_database_api } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // const params = '?language=pt-br';
+    const api_key = the_movie_database_api.key;
+    const baseUrl = the_movie_database_api.baseUrlv3;
+
+    // headers: req.headers.set('Authorization', `Bearer ${api_key}`),
+    const newReq = req.clone({
+      responseType: 'json',
+      setHeaders: {
+        'Authorization': `Bearer ${api_key}`,
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+      url: baseUrl + req.url,
+    });
+
+    return next.handle(newReq);
+  }
+
+}
